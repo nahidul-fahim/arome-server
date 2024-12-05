ed27f74bf559b4ce2b90989a06693be9408d3318
nahidul-fahim <nahidul7562@gmail.com>
2024-12-05 23:53:24 +0600

    added model, app and server file

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/global-error-handler';
+import router from './app/routes';
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Library Management System is running!");
+})
+
+app.use('/api', router);
+
+app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        success: false,
+        message: "API NOT FOUND!",
+        error: {
+            path: req.originalUrl,
+            message: "Your requested path is not found!"
+        }
+    })
+})
+
+export default app;
